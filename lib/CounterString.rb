5b087d94957d658fd69f1a234d2920c101c10129
Chris Morris <chris.morris@livingsocial.com>
2012-05-31 20:45:53 -0500

    Reworked algorithm to behave the same way as the original counterstring command in Perlclip. Now instead of generate(9) which returned a string with the correct length but without the correct number displayed 2*4*6*8*1 it returns *3*5*7*9*. Special case generate(1) returns * instead of 1, also matching Perlclip.

--- lib/CounterString.rb
+++ lib/CounterString.rb
@@ -1,33 +1,17 @@
 class CounterString
   def initialize()
     @position_marker_character = "*"
-    @output = ""
   end
 
   def generate(length=0)
-	length = length.to_i
-    if length == 0
-      return ""
-    elsif length == 1
-      return "#{length}"
-    else
-      @output = "2" + @position_marker_character
-      last_position_number = 2
-      while @output.size < length
-        position_number = @output.size + (last_position_number.to_s.size + 1)
-        if position_number.to_s.size > last_position_number.to_s.size
-          position_number += 1
-        end
-        token = position_number.to_s + @position_marker_character 
-        remaining_length = length - @output.size
-        if remaining_length < token.size + @output.size 
-          token = token[0,remaining_length]
-        end
-        @output += token
-        last_position_number = position_number 
-      end
+    output = ''
+    length = length.to_i
+    while output.length < length
+      diff = length - output.length
+      output = "*" + output
+      output = ((diff > 1) ? diff.to_s : "") + output
     end
-    return @output
+    output
   end
 
 end
